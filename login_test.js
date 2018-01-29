66cb56825d4e9fcb1663444dfa59519ec72d6768
Katarzyna Ulok <katarzyna.ulok@gmail.com>
2018-01-29 15:28:12 +0100

    Using before suite and before to clean form validation

--- login_test.js
+++ login_test.js
@@ -2,21 +2,30 @@
 
 Feature('Login');
 
+BeforeSuite((I) => {
+    I.amOnPage('/accounts/en-US/login');
+});
+
+Before((I) => {
+    I.refreshPage();
+});
 
 Scenario('email and password are required', (I) => {        
-    I.amOnPage('https://www.everreal.co/accounts/en-US/login');
     I.click('Login');
-    I.see('Field is required')
+    I.see('Field is required');
 });
 
 Scenario('required correct email format', (I) => {
+    I.fillField('E-Mail', 'somethingtotest');
+    I.click('Login');
+    I.see('Email is not correct');
 });
 
-Scenario('password is required', (I) => {
-});
+// Scenario('password is required', (I) => {
+// });
 
-Scenario('email is required', (I) => {
-});
+// Scenario('email is required', (I) => {
+// });
 
-Scenario('wrong credentials', (I) => {
-});
+// Scenario('wrong credentials', (I) => {
+// });
